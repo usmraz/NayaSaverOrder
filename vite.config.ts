bcad25266a64396f6e5978ad704aacd21b2f5058
usmraz <usmraz@gmail.com>
2025-12-29 10:45:16 +0500

    Update Vite configuration with base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', 
+})
+
+
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
