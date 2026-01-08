1337bbdfdbf5172f9fd456653b1207c9de9cfd25
usmraz <usmraz@gmail.com>
2026-01-08 15:34:52 +0500

    feat: Update Android project dependencies and configuration
    Removes unused Capacitor Android dependencies and updates project configuration. This commit also cleans up the Android project's .gitignore and build files, preparing for streamlined builds and dependency management.

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nayaorder.app',
-  appName: 'NSPRO',
-  webDir: 'dist'
-};
-
-export default config;
